f38c299f03f2c69a9e902d5567499d7be40726b4
Mohon45 <mdmohon6145@gmail.com>
2022-03-21 01:30:49 +0600

    some bug fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Services from './components/Services/Services';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import NotFound from './components/NotFound/NotFound';
@@ -20,9 +19,6 @@ function App() {
           </Route>
           <Route path="/home">
             <Home></Home>
-          </Route>
-          <Route path="/service">
-            <Services></Services>
           </Route>
           <Route path="/about">
             <About></About>
